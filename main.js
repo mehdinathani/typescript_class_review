0a7888011f5d41c633d512523c73e75bb6b08da5
mehdinathani <mehdinathani@gmail.com>
2024-02-24 16:33:55 +0500

    first commit

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 "use strict";
+// camel
 let firstName = "Student";
 let age = 20;
 age = 21;
 age = 22;
 console.log(age);
-// var obsulte
-const cnicNumber = "";
+// var obsulte = dont use
+const cnicNumber = "423012222222";
 console.log("Hello World");
 //operators + - * / ** % < >
-let num1 = 4;
+let num1 = 5;
 let num2 = 2;
 let isHappy = true;
 let isSubscribe = true;
@@ -16,9 +17,12 @@ let isSubscribe = true;
 let result = num1 + num2;
 console.log(result);
 console.log(`Is Subscribe =  ${isSubscribe}`);
-console.log(`Addition of 2 values = 4 + 2 = ${num1 + num2}`);
-console.log(`Substraction of 2 values = 4 - 2 =  ${num1 - num2}`);
-console.log(`Multiplication of 2 values = 4 * 2 = ${num1 * num2}`);
-console.log(`Divide of 2 values = 4 / 2 = ${num1 / num2}`);
-console.log(`Exponent (Power) of 2 values = 4 power 2 = ${num1 ** num2}`);
-console.log(`Moduler of 2 values = 4 modular 2 = ${num1 % num2}`);
+console.log(`Addition of 2 values = 5 + 2 = ${num1 + num2}`);
+console.log(`Substraction of 2 values = 5 - 2 =  ${num1 - num2}`);
+console.log(`Multiplication of 2 values = 5 * 2 = ${num1 * num2}`);
+console.log(`Divide of 2 values = 5 / 2 = ${num1 / num2}`);
+console.log(`Exponent (Power) of 2 values = 5 power 2 = ${num1 ** num2}`);
+console.log(`Moduler of 2 values = 5 modular 2 = ${num1 % num2}`);
+let number1 = "GIAIC";
+let number2 = "Hamza";
+console.log(number1 + number2);
--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
+// camel
+
+
 let firstName = "Student";
 let age = 20;
 age = 21;
 age = 22;
 console.log(age);
-// var obsulte
-const cnicNumber = "";
+// var obsulte = dont use
+const cnicNumber = "423012222222";
+
 console.log("Hello World");
 
 //operators + - * / ** % < >
 
-let num1: number = 4;
+let num1: number = 5;
 let num2: number = 2;
 
 let isHappy:boolean = true;
@@ -21,9 +25,14 @@ console.log(result);
 
 console.log(`Is Subscribe =  ${isSubscribe}`);
 
-console.log(`Addition of 2 values = 4 + 2 = ${num1 + num2}`);
-console.log(`Substraction of 2 values = 4 - 2 =  ${num1 - num2}`);
-console.log(`Multiplication of 2 values = 4 * 2 = ${num1 * num2}`);
-console.log(`Divide of 2 values = 4 / 2 = ${num1 / num2}`);
-console.log(`Exponent (Power) of 2 values = 4 power 2 = ${num1 ** num2}`);
-console.log(`Moduler of 2 values = 4 modular 2 = ${num1 % num2}`);
+console.log(`Addition of 2 values = 5 + 2 = ${num1 + num2}`);
+console.log(`Substraction of 2 values = 5 - 2 =  ${num1 - num2}`);
+console.log(`Multiplication of 2 values = 5 * 2 = ${num1 * num2}`);
+console.log(`Divide of 2 values = 5 / 2 = ${num1 / num2}`);
+console.log(`Exponent (Power) of 2 values = 5 power 2 = ${num1 ** num2}`);
+console.log(`Moduler of 2 values = 5 modular 2 = ${num1 % num2}`);
+
+let number1 = "GIAIC";
+let number2 = "Hamza";
+
+console.log(number1+number2);
