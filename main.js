8a1a53a1e641317a7f5717a95ee938f94565fa09
mehdinathani <mehdinathani@gmail.com>
2024-02-24 16:16:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+let firstName = "Student";
+let age = 20;
+age = 21;
+age = 22;
+console.log(age);
+// var obsulte
+const cnicNumber = "";
+console.log("Hello World");
+//operators + - * / ** % < >
+let num1 = 4;
+let num2 = 2;
+let isHappy = true;
+let isSubscribe = true;
+//          1   +      2 = 12
+let result = num1 + num2;
+console.log(result);
+console.log(`Is Subscribe =  ${isSubscribe}`);
+console.log(`Addition of 2 values = 4 + 2 = ${num1 + num2}`);
+console.log(`Substraction of 2 values = 4 - 2 =  ${num1 - num2}`);
+console.log(`Multiplication of 2 values = 4 * 2 = ${num1 * num2}`);
+console.log(`Divide of 2 values = 4 / 2 = ${num1 / num2}`);
+console.log(`Exponent (Power) of 2 values = 4 power 2 = ${num1 ** num2}`);
+console.log(`Moduler of 2 values = 4 modular 2 = ${num1 % num2}`);
---
+++ main.ts
@@ -0,0 +1,29 @@
+let firstName = "Student";
+let age = 20;
+age = 21;
+age = 22;
+console.log(age);
+// var obsulte
+const cnicNumber = "";
+console.log("Hello World");
+
+//operators + - * / ** % < >
+
+let num1: number = 4;
+let num2: number = 2;
+
+let isHappy:boolean = true;
+let isSubscribe: boolean = true;
+//          1   +      2 = 12
+let result = num1 + num2;
+
+console.log(result);
+
+console.log(`Is Subscribe =  ${isSubscribe}`);
+
+console.log(`Addition of 2 values = 4 + 2 = ${num1 + num2}`);
+console.log(`Substraction of 2 values = 4 - 2 =  ${num1 - num2}`);
+console.log(`Multiplication of 2 values = 4 * 2 = ${num1 * num2}`);
+console.log(`Divide of 2 values = 4 / 2 = ${num1 / num2}`);
+console.log(`Exponent (Power) of 2 values = 4 power 2 = ${num1 ** num2}`);
+console.log(`Moduler of 2 values = 4 modular 2 = ${num1 % num2}`);
